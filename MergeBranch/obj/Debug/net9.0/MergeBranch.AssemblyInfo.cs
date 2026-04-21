172eff53b334f5e58fd22e4b96b1169212f82e4c
Jayden Sakow <jaydensakow@gmail.com>
2026-04-21 15:10:52 -0700

    Added chimp code

--- MergeBranch/obj/Debug/net9.0/MergeBranch.AssemblyInfo.cs
+++ MergeBranch/obj/Debug/net9.0/MergeBranch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MergeBranch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055ef4ce7fa6580eb0266884c2ec7406bbb8677d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MergeBranch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MergeBranch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
